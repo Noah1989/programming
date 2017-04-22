891ebd1a7f8345fa3f62ee24bc8eaf755b1526aa
Stefan Paul Noack <stefan@noack-software.de>
2017-04-22 18:39:09 +0200

    Add ucsim package for Travis CI, show ucsim version, add .gitignore.

--- run51.sh
+++ run51.sh
@@ -1,6 +1,5 @@
 #! /bin/sh
-
-{ (echo dc 0 0; yes step) \
+s51 -v >&2 && { (echo dc 0 0; yes step) \
 	| s51 -S out=/proc/self/fd/3 "$1" \
 	| grep -oP '(0>)? \?? \K0x.*$' \
 	| sed '2,${/^0x0000/q}' > trace.log \
